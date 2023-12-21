32de0cf624c1b8b3847ba588b6c76260b551e37b
ryanbakker <rmbakker2002@gmail.com>
2023-12-22 00:54:40 +1300

    First Commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
---
+++ app/(root)/meets/[id]/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
+function Meets() {
+  return (
+    <div>
+      <UserButton />
+      <h2>Car Meet</h2>
+    </div>
+  );
+}
+
+export default Meets;
